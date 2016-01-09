6c676545d1631599509af32392e5cf7c45a74a3d
robst <robertst81@gmail.com>
2016-01-09 10:37:54 +0100

    added monitor class to handle processmanagment

--- lib/thread_watcher.rb
+++ lib/thread_watcher.rb
@@ -1,5 +1,21 @@
 require "thread_watcher/version"
 
 module ThreadWatcher
-  # Your code goes here...
+  require 'singleton'
+  class Monitor
+    include Singleton
+
+    def initialize
+      @process_watch = ThreadWatcher::ProcessWatch.new
+    end
+
+    def run &block
+      @process_watch.run &block
+    end
+
+    def kill id
+      @process_watch.kill id
+    end
+
+  end
 end
