f4b5db06c128b79ad285aba8ca790697272eebad
robst <robertst81@gmail.com>
2016-01-09 10:31:12 +0100

    added process_watch for handling threads

---
+++ lib/thread_watcher/process_watch.rb
@@ -0,0 +1,42 @@
+module ThreadWatcher
+  class ProcessWatch
+    attr_accessor :threads
+    class ThreadHoler
+      attr_accessor :thread, :id
+      def initialize thread
+        @thread = thread
+        @id = Time.now.to_i
+      end
+
+      def stop!
+        @thread.kill
+      end
+
+      def alive?
+        @thread.alive?
+      end
+    end
+
+    def initialize
+      @threads = {}
+      run { while true; self.clear!; sleep(60); end; }
+    end
+
+    def run &block
+      thread_holder = ThreadHoler.new(Thread.new { block.call })
+      @threads[thread_holder.id] = thread_holder
+    end
+
+    def kill id
+      @threads[id].stop!
+      @threads.delete id
+    end
+
+    def clear!
+      @threads.each do |key, thread|
+        next if thread.alive?
+        kill key
+      end
+    end
+  end
+end
